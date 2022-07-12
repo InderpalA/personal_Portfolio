9ce2ddee19b35d3cbdae92a90963fd4de89c531a
Inderpal Aubby <inderpalaubby@gmail.com>
2022-07-12 17:13:50 +0100

    V3 - Addition of Interactivity

---
+++ script.js
@@ -0,0 +1,26 @@
+//First Project
+let eventTarget1 = document.getElementById('rpsls');
+
+function alertrpsls(){
+    alert("Classic Rock Paper Scissors game with the addition of 2 new choices. You play against the computer which randomly generates guesses")
+}
+
+eventTarget1.addEventListener('click', alertrpsls);
+
+//Second Project
+let eventTarget2 = document.getElementById('mixed-messages');
+
+function alertMixedMessages(){
+    alert("Program which creates random sayings!")
+}
+
+eventTarget2.addEventListener('click', alertMixedMessages);
+
+//Third Project
+let eventTarget3 = document.getElementById('cheatsheet');
+
+function alertcheatsheet(){
+    alert("I created my own CheatSheet when learning CSS")
+}
+
+eventTarget3.addEventListener('click', alertcheatsheet);
